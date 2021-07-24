1c459570cca2ee7199253d512d2d461b827ae68f
Viraj-Vijay-Hire <78733888+Viraj-Vijay-Hire@users.noreply.github.com>
2021-07-24 18:00:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 song = "";
 
+rightWrist_X = 0;
+rightWrist_Y = 0;
+
+leftWrist_X = 0;
+leftWrist_Y = 0;
+
 function preload()
 {
     song = loadSound("music.mp3");
@@ -12,6 +18,25 @@ function setup()
     
     video = createCapture(600, 500);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWrist_X = results[0].pose.leftWrist.x;
+        leftWrist_Y = results[0].pose.leftWrist.y;
+        console.log(leftWrist_X, leftWrist_Y);
+
+        rightWrist_X = results[0].pose.rigthWrist.x;
+        rightWrist_Y = results[0].pose.rightWrist.y; 
+        console.log(rightWrist_X, rightWrist_Y);
+    }    
 }
 
 function draw()
@@ -22,4 +47,12 @@ function draw()
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
+
+function modelLoaded()
+{
+    console.log("PoseNet is initialized");
+}
+
